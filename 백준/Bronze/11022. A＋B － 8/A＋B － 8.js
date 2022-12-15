740d70b769836ffb3e872c02594b2e4517269ebd
SoJin <81489300+jinist94@users.noreply.github.com>
2022-12-16 04:15:11 +0900

    [Bronze V] Title: A+B - 8, Time: 116 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
+const input = fs.readFileSync(path).toString().split("\n").map(el=> el.split(' '));
+
+
+const length = input[0][0];
+let answer = '';
+
+for(let i = 1; i <= length; i++){
+  const A = parseInt(input[i][0]);
+  const B = parseInt(input[i][1]);
+
+  answer += `Case #${i}: ${A} + ${B} = ${A + B}\n`;
+}
+
+console.log(answer);
