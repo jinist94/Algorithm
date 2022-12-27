6cfc29f5a9649afd4bca96dea1acc0766c9dc4c9
SoJin <81489300+jinist94@users.noreply.github.com>
2022-12-28 01:21:26 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 136 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
+const input = fs.readFileSync(path).toString();
+
+const map = {
+  a: -1,
+  b: -1,
+  c: -1,
+  d: -1,
+  e: -1,
+  f: -1,
+  g: -1,
+  h: -1,
+  i: -1,
+  j: -1,
+  k: -1,
+  l: -1,
+  m: -1,
+  n: -1,
+  o: -1,
+  p: -1,
+  q: -1,
+  r: -1,
+  s: -1,
+  t: -1,
+  u: -1,
+  v: -1,
+  w: -1,
+  x: -1,
+  y: -1,
+  z: -1,
+};
+
+for (let i = 0; i < input.length; i++) {
+  if (map[input[i]] === -1) {
+    map[input[i]] = i;
+  }
+}
+
+console.log(Object.values(map).join(" "));
